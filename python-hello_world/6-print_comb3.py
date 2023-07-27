82e76e89427fc685863e82049ab47336c5a7f1b3
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-07-27 08:58:41 -0700

    print combo3

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,6 @@
+for x in range(10):
+    for y in range(x + 1, 10):
+        if x < 8:
+            print("{:0d}{:0d}".format(x, y), end=", ")
+        else:
+         print("{:0d}{:0d}".format(x, y))
