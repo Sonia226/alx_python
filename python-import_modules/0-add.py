9fa3276d0b0ee3912456622b28c9ff847bf7d6bb
Sonia226 <tsekpodoesoniaselikem@gmail>
2023-08-04 11:37:04 -0700

    add update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
-from add_0 import add
+add = __import__("add_0").add
  
 a = 1
 b = 2
  
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+print("{} + {} = {}".format(a, b, add(a, b)))
