f5ea11d7a5e26585a8479a8d886c5cad7b382c27
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-09-12 23:51:04 -0700

    0-square update & test file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,8 +4,8 @@
 class Square:
     #Representing a square in a class
 
-    def __init__(self, size)
-    #Initiating the size
-    #Args;
-    #size (int): The size of the square
-    self.__size = size
+    def __init__(self, size):
+        #Initiating the size
+        #Args;
+        #size (int): The size of the square
+        self.__size = size
