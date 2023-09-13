0f3d3dfe07d7a534bf1b1a81d741bf1aa67aadd9
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-09-13 00:37:03 -0700

    update 0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,13 @@
-#Class Module.
+'''Class Module.'''
 
 
 class Square:
-    #Representing a square in a class.
+    '''Representing a square in a class.'''
+
 
     def __init__(self, size):
-        #Initiating the size
-        #Args:
-        #size (int): The size of the square
+        '''Initiating the size
+
+        Args:
+        size (int): The size of the square'''
         self.__size = size
