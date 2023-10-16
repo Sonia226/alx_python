7d1095d2e5b018a9665f8b45821da32f39eeb616
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-10-15 23:30:40 -0700

    redo task 3

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,13 +1,27 @@
 """
-This module sends a request to the URL 
-and displays the value of the variable X-Request-Id
+This module takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter
 """
 import requests
 import sys
 
-if __name__ == "__main__":
 
-    url = sys.argv[1]
-    r = requests.get(url)
-    r1 = r.headers.get("X-Request-Id", None)
-    print(r1)
+def send_post_request(url, email):
+    try:
+        data = {'email': email}
+        response = requests.post(url, data=data)
+        if response.status_code == 200:
+            print(response.text)
+        else:
+            print(f'Request failed with status code: {response.status_code}')
+    except requests.exceptions.RequestException as e:
+        print(f'An error occurred: {e}')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <URL> <email>")
+    else:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        send_post_request(url, email)
